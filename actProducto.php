09f4fb6f0814c82d5eaf3747d6aaad9539edeb62
abelalbala <cf21abel.albaladejo@iesjoandaustria.org>
2022-12-05 19:37:18 +0100

    al dia

--- actProducto.php
+++ actProducto.php
@@ -1,13 +1,26 @@
+
+
 <?php
 
-echo "hola";
 echo $_POST["nomProducto"];
+echo "<br>";
 print_r($_FILES["inputFiles"]);
+echo "<br><br>";
  
 for ($i=0; $i<count($_FILES["inputFiles"]["name"]); $i++) {
-    echo $_FILES["inputFiles"]["tmp_name"][$i] .'<br>';
-    echo $_FILES["inputFiles"]["name"][$i] .'<br>';
+    $archivo = $_FILES['inputFiles']['name'][$i];
+    $tamano = $_FILES['inputFiles']['size'][$i];
+    $temp = $_FILES['inputFiles']['tmp_name'][$i];
+    echo $temp.'<br>';
+    echo "Name: ".$archivo.'<br>';
+    if (move_uploaded_file($temp, 'public/'.$archivo)) {
+        chmod('public/'.$archivo, 0777);
+        echo '<div><b>Se ha subido correctamente la imagen.</b></div>';
+        echo '<p><img src="public/'.$archivo.'"></p>';
+    }
+    else {
+       echo '<div><b>Ocurrió algún error al subir el fichero. No pudo guardarse.</b></div>';
+    }
 }
-
 
 ?>
